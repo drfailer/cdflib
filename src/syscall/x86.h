aae29925b756725d2ac3adc8b106202084a0484a
drfailer <drfailer.42@gmail.com>
2024-04-04 12:11:20 +0200

    argument register for x86

--- src/syscall/x86.h
+++ src/syscall/x86.h
@@ -5,6 +5,14 @@
 #define SYS_READ 0x03
 #define SYS_WRITE 0x04
 
+#define rax "rax"
+#define ARG_0 "ebx"
+#define ARG_1 "ecx"
+#define ARG_2 "edx"
+#define ARG_3 "esi"
+#define ARG_4 "edi"
+#define ARG_5 "edp"
+
 #define stdin 0
 #define stdout 1
 #define stderr 2
