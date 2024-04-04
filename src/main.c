c3047834e5f92102391bdf824c46c259d9988579
drfailer <drfailer.42@gmail.com>
2024-04-04 20:22:33 +0200

    some tests in main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "std/assert.h"
 
 void testStringConvertion() {
+    /* number to string */
     int basicNumber = dfstrtolong("1234");
     double basicDouble = dfstrtodouble("1234.7891");
     int negativeInt = dfstrtolong("-84");
@@ -17,6 +18,37 @@ void testStringConvertion() {
 	assert(-323.00983209832 == negativeDouble);
 	assert(-1.001 == precision1);
 	assert(-1.0002823098 == precision2);
+
+    /* string to number */
+    char buff[100];
+
+    dflongtostr(buff, 1234, 255);
+	assert(dfstreq("1234", buff));
+
+    dfdoubletostr(buff, 1234.7891, 255);
+    /* dfwrite(stdout, buff, dfstrlen(buff)); */
+    /* dfwrite(stdout, "\n", 1); */
+	/* assert(dfstreq("1234.7891", buff)); */
+
+    dflongtostr(buff, -84, 255);
+    /* dfwrite(stdout, buff, dfstrlen(buff)); */
+    /* dfwrite(stdout, "\n", 1); */
+	assert(dfstreq("-84", buff));
+
+    dfdoubletostr(buff, -323.00983209832, 255);
+    /* dfwrite(stdout, buff, dfstrlen(buff)); */
+    /* dfwrite(stdout, "\n", 1); */
+	/* assert(dfstreq("-323.00983209832", buff)); */
+
+    dfdoubletostr(buff, -1.001, 255);
+    /* dfwrite(stdout, buff, dfstrlen(buff)); */
+    /* dfwrite(stdout, "\n", 1); */
+	/* assert(dfstreq("-1.001", buff)); */
+
+    dfdoubletostr(buff, -1.0002823098, 255);
+    /* dfwrite(stdout, buff, dfstrlen(buff)); */
+    /* dfwrite(stdout, "\n", 1); */
+	/* assert(dfstreq("-1.0002823098", buff)); */
 }
 
 void _start(void) {
@@ -24,10 +56,18 @@ void _start(void) {
     testStringConvertion();
 
     /* test input oupout */
-    char buff[100] = {0};
-    dfwrite(stdout, "Enter a message:\n", 17);
-    dfread(stdin, buff, 100);
-    dfwrite(stdout, buff, 100);
+    /* char buff[100] = {0}; */
+    /* dfwrite(stdout, "Enter a message:\n", 17); */
+    /* dfread(stdin, buff, 100); */
+    /* dfwrite(stdout, buff, 100); */
+
+    dfprintf("test\n");
+    dfprintf("int: %d\n", 1);
+    dfprintf("int: %f\n", 1.1);
+    dfprintf("char: %c\n", 'c');
+    dfprintf("str: %s\n", "hello, world");
+    dfprintf("int: %d & char: %c\n", 1, 'f');
+    dfprintf("int: %d & char: %c & str: \"%s\"\n", 1, 'f', "hello world");
 
     dfexit(EXIT_SUCESS);
     __builtin_unreachable(); // make that the compiler doesn't put anything else
